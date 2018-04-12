bea75c29203ebc5c0a4d3bc0dd7d3148833e4052
RoccoDev <roccodev.business@gmail.com>
2018-04-12 19:15:41 +0200

    Maybe now?

---
+++ src/main/kotlin/tk/roccodev/hiveapi/player/stats/DR.kt
@@ -0,0 +1,21 @@
+package tk.roccodev.hiveapi.player.stats
+import tk.roccodev.hiveapi.player.PVPStats
+
+/**
+ * Main BedWars data fetcher
+ *
+ * Inherits data from PVPStats (kills, deaths)
+ *
+ *
+ */
+class BED(player: String) : PVPStats("BED", player) {
+
+
+
+    val bedsDestroyed
+                get() = jsonObj.int("beds_destroyed")
+
+    val teamsEliminated
+                get() = jsonObj.int("teams_eliminated")
+
+}
